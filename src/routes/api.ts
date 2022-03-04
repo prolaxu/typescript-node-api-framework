da2a761735ffa2d399f81742c009a6c4e15cef4d
prolaxu <adhikarilaxu0001@gmail.com>
2022-03-04 16:41:19 +0545

    enhc -  user login regerstration.

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,14 +1,23 @@
 import {Router} from 'express';
 import HomeController from '../app/controller/HomeController';
 import PostController from '../app/controller/PostController';
+import AuthController from '../app/controller/AuthController';
 import crud from '../core/utills/crud';
+import {authuser} from '../app/middleware/authuser';
+
 
 const router = Router();
 const home = new HomeController();
+const auth = new AuthController();
 
 router.get('/', (res,req)=>home.index(res,req));
 // route for post crud
 router.use('/', crud('post',router,new PostController));
+
+// Api Auth
+router.post('/login', (req,res)=>auth.login(req,res));
+router.post('/register', (req,res)=>auth.register(req,res));
+router.get('/encrypt',authuser, (req,res)=>auth.encrypt(req,res));
 
 // Export the router
 const  api:Router = router;
